ddc3150618599e377045a1c1679552afcee86fb9
Missingrakan <3047451493@qq.com>
2019-12-02 23:37:24 +0800

    更新代码

--- test_12_1/test_12_1/test.cpp
+++ test_12_1/test_12_1/test.cpp
@@ -3,10 +3,11 @@
 #include <iostream>
 using namespace std;
 
+/*
 class Int
 {
-	friend ostream& operator<<(ostream& out,const Int i);
-	friend istream& operator>>(istream& in, Int i);
+	friend ostream& operator<<(ostream& out,const Int x);
+	friend istream& operator>>(istream& in,Int x);
 
 public:
 	Int(int i = 0) : m_i(i)
@@ -80,24 +81,66 @@ public:
 	}
 	Int operator++(int)		//i++	tmp为临时对象,不能以引用返回.
 	{
-		/*Int tmp(m_i);
-		m_i++;
-		return tmp;*/
+		//Int tmp(m_i);
+		//m_i++;
+		//return tmp;
 		Int tmp(*this);
 		++*this;
 		return tmp;
 	}
-	Int& operator--();		//++i
-	Int operator--(int);	//++i
-
+	Int& operator--()		//--i
+	{
+		m_i--;
+		return *this;
+	}
+	Int operator--(int)		//--i
+	{
+		Int tmp(*this);
+		--*this;
+		return tmp;
+	}
+public:
+	bool operator==(const Int &x)
+	{
+		return m_i == x.m_i;
+	}
+	bool operator!=(const Int &x)
+	{
+		//return m_i != x.m_i;
+		return !(*this == x);
+	}
+	bool operator>(const Int &x)
+	{
+		return m_i > x.m_i;
+	}
+	bool operator<=(const Int &x)
+	{
+		//return m_i <= x.m_i;
+		return !(*this > x);
+	}
+	bool operator<(const Int &x)
+	{
+		return m_i < x.m_i;
+	}
+	bool operator>=(const Int &x)
+	{
+		//return m_i >= x.m_i;
+		return !(*this < x);
+	}
 private:
 	int m_i;
 };
 
-ostream& operator<<(ostream& out, const Int i)
+ostream& operator<<(ostream& out, const Int x)
 {
-	out<< i.m_i;
+	out<< x.m_i;
 	return out;
+}
+
+istream& operator>>(istream& in, Int x)
+{
+	in >> x.m_i;
+	return in;
 }
 
 void main()
@@ -126,4 +169,78 @@ void main()
 	
 	cout << i1 << endl;
 
+}
+*/
+
+class Complex
+{
+	friend ostream& operator<<(ostream &out, const Complex &c);
+	friend Complex operator+(int x, const Complex &c);
+public:
+	Complex(int real = 0, int imag = 0) : m_real(real), m_imag(imag)
+	{}
+	~Complex()
+	{}
+public:
+	int GetReal()const
+	{return m_real;}
+	int  GetImag()const
+	{return m_imag;}
+public:
+	//a + b		a.operator(b) ==>operate+(&a,b);
+	Complex operator+(const Complex &c)
+	{
+		Complex tmp(m_real + c.m_real, m_imag + c.m_imag);
+		return tmp;
+		//无名临时对象的语法
+		//return Complex(m_real+c.m_real,m_imag+c.m_imag);
+	}
+	Complex operator-(const Complex &c)
+	{
+		Complex tmp(m_real - c.m_real, m_imag - c.m_imag);
+		return tmp;
+		//无名临时对象的语法
+		//return Complex(m_real-c.m_real,m_imag-c.m_imag);
+	}
+	//c + x
+	Complex operator+(int x)
+	{
+		Complex tmp(m_real+x, m_imag);
+		return tmp;
+	}
+	//a += b
+	Complex& operator+=(const Complex &c)
+	{
+		m_real += c.m_real;
+		m_imag += c.m_imag;
+		return *this;
+	}
+private:
+	int m_real;
+	int m_imag;
+};
+
+//ostream cout
+ostream& operator<<(ostream &out, const Complex &c)
+{
+	out << "(" << c.GetReal() << ":" << c.GetImag() << ")";
+	return out;
+}
+Complex operator+(int x, const Complex &c)
+{
+	Complex tmp(c.m_real + x, c.m_imag);
+	return tmp;
+}
+void main()
+{
+	Complex c1(1, 2);
+	Complex c2(2, 3);
+	Complex c(0, 0);
+	c = c1 + c2;
+	cout << c << endl;
+	c1 += c2;
+	cout << c1 << endl;
+	//c = c2 + 3;  // c + c
+	c = 3 + c2;
+	cout << c << endl;
 }
