ea37f77fb598bf82c07ef5ff5daa1a931d2a82da
Missingrakan <3047451493@qq.com>
2020-03-14 15:43:44 +0800

    更新代码

--- STL/IO_jicheng/IO_jicheng/test.cpp
+++ STL/IO_jicheng/IO_jicheng/test.cpp
@@ -1,52 +1,100 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include <iostream>
+#include <string>
 #include <fstream>
 #include <sstream>
 using namespace std;
 
-struct ServerInfo
-{
-	char _ip[30];	//ip		192.168.10.100
-	int _port;		//端口		8080
-};
-class ConfigManager
-{
-public:
-	ConfigManager(const string cfgfile) :_configFile(cfgfile)
-	{}
-public:
-	//文本
-	void Write_ServerInfo_Test(const ServerInfo &info)
-	{
-		ofstream ofile(_configFile, ios::out);
-		if (!ofile)
-			return;
-		ofile << info._ip << " " << info._port;
-		ofile.close();
-	}
-	void Read_ServerInfo_Test(ServerInfo &info)
-	{
-		ifstream ifile(_configFile, ios::in);
-		if (!ifile)
-			return;
-		ifile >> info._ip >> info._port;
-		ifile.close();
-	}
-private:
-	string _configFile;
-};
-
 int main()
 {
-	ServerInfo serve_info = { "192.168.10.100", 8080 };
-	ConfigManager cfgmgr("bitServer.config");
-	cfgmgr.Write_ServerInfo_Test(serve_info);
-	ServerInfo tmp_info;
-	cfgmgr.Read_ServerInfo_Test(tmp_info);
-	cout << tmp_info._ip << " : " << tmp_info._port << endl;
+	stringstream sstream;		//空间中转站
+	sstream << "first" << " " << "string.";
+	sstream << " second string";
+	//first string second string
+	cout << "strResult is: " << sstream.str() << endl;
+	//清空 sstream
+	sstream.str("");
+	sstream << "third string";
+	cout << "After clear,strResult is: " << sstream.str() << endl;
 	return 0;
 }
+
+//int main()
+//{
+//	int a = 1234560;
+//	string str;
+//	stringstream s; //string 对象 扩容 内存不足
+//	s << a;		//12345  -> str;
+//	s >> str;
+//	cout << "str = " << endl;
+//	s.clear();
+//	double d = 12.345;
+//	s << d;
+//	s >> str;
+//	cout << "str = " << str << endl;
+//	string value = s.str();
+//	cout << "value = " << value << endl;
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = -123;
+//	char s1[32];
+//	_itoa(n, s1, 10);
+//	cout << s1 << endl;
+//	char s2[32];
+//	sprintf(s2, "%d", n);
+//	cout << s2 << endl;
+//	char s3[32];
+//	sprintf(s3, "%f", n);
+//	cout << s3 << endl;
+//	return 0;
+//}
+
+//struct ServerInfo
+//{
+//	char _ip[30];	//ip		192.168.10.100
+//	int _port;		//端口		8080
+//};
+//class ConfigManager
+//{
+//public:
+//	ConfigManager(const string cfgfile) :_configFile(cfgfile)
+//	{}
+//public:
+//	//文本
+//	void Write_ServerInfo_Test(const ServerInfo &info)
+//	{
+//		ofstream ofile(_configFile, ios::out);
+//		if (!ofile)
+//			return;
+//		ofile << info._ip << " " << info._port;
+//		ofile.close();
+//	}
+//	void Read_ServerInfo_Test(ServerInfo &info)
+//	{
+//		ifstream ifile(_configFile, ios::in);
+//		if (!ifile)
+//			return;
+//		ifile >> info._ip >> info._port;
+//		ifile.close();
+//	}
+//private:
+//	string _configFile;
+//};
+//
+//int main()
+//{
+//	ServerInfo serve_info = { "192.168.10.100", 8080 };
+//	ConfigManager cfgmgr("bitServer.config");
+//	cfgmgr.Write_ServerInfo_Test(serve_info);
+//	ServerInfo tmp_info;
+//	cfgmgr.Read_ServerInfo_Test(tmp_info);
+//	cout << tmp_info._ip << " : " << tmp_info._port << endl;
+//	return 0;
+//}
 
 //int main()
 //{
