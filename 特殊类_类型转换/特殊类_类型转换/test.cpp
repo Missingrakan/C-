ec95cd90c431e71da2d23633ccb8f1c186e27e2c
Missingrakan <3047451493@qq.com>
2020-09-08 20:19:01 +0800

    更新代码

--- 特殊类_类型转换/特殊类_类型转换/test.cpp
+++ 特殊类_类型转换/特殊类_类型转换/test.cpp
@@ -2,32 +2,265 @@
 
 #include <iostream>
 #include <memory>
+#include <mutex>
+#include <thread>
+#include <assert.h>
 
 using namespace std;
 
-class Test
-{
-public:
-	static Test* GetInstance()
-	{
-		return new Test;
-	}
-public:
-	void fun()
-	{
-		cout << "Test::fun()" << endl;
-	}
-private:
-	Test()
-	{}
-private:
-	int m_data = 0;
-};
 void main()
 {
-	Test *pt = Test::GetInstance();
-	pt->fun();
+	const int a = 10;
+	int *p = const_cast<int*>(&a);
+	int &b = const_cast<int&>(a);
+
+	volatile int x = 20;			
+	int *px = const_cast<int*>(&x);	//去掉volatile
 }
+
+//void main()
+//{
+//	int a = 10;
+//	double d = 12.34;
+//	a = static_cast<int>(d);
+//	int p = 0x0012ff7c;
+//	int *ptr = static_cast<int*>(p);		//err
+//
+//	//int a = 10;
+//	//double d = 12.34;
+//	//a = d;		//隐式转换
+//	//a = (int)d;	//显式转换
+//	//int *p = (int*)0x0012ff7c;
+//}
+
+//template<typename T>
+//class LASingletonTemplateBase
+//{
+//private:
+//	static T* sm_instance;
+//protected:
+//	LASingletonTemplateBase()
+//	{
+//		assert(sm_instance == 0);
+//		sm_instance = static_cast<T*>(this);
+//	}
+//	virtual ~LASingletonTemplateBase()
+//	{
+//		assert(sm_instance != 0);
+//		sm_instance = 0;
+//	}
+//public:
+//	static T* get_instance_ptr()
+//	{
+//		if (sm_instance == 0)
+//		{
+//			sm_instance = new T();
+//		}
+//		return sm_instance;
+//	}
+//	static T& get_instance_ref()
+//	{
+//		if (sm_instance == 0)
+//		{
+//			sm_instance = new T();
+//		}
+//		return *sm_instance;
+//	}
+//	static void remove_instance()
+//	{
+//		assert(sm_instance);
+//		if (sm_instance)
+//		{
+//			delete sm_instance;
+//		}
+//		assert(sm_instance == 0);
+//	}
+//};
+//template<typename T>
+//T* LASingletonTemplateBase<T>::sm_instance = 0;
+//class Test
+//{};
+//void main()
+//{
+//	Test t1, t2, t3;
+//	Test *pt1 = LASingletonTemplateBase<Test>::get_instance_ptr();
+//	Test *pt2 = LASingletonTemplateBase<Test>::get_instance_ptr();
+//	Test *pt3 = LASingletonTemplateBase<Test>::get_instance_ptr();
+//}
+
+//mutex mt;
+//class Singleton
+//{
+//public:
+//	static Singleton* Instance()
+//	{
+//		if (_instance == nullptr)
+//		{
+//			mt.lock();
+//			if (_instance == nullptr)
+//			{
+//				_instance = new Singleton;
+//			}
+//			mt.unlock();
+//		}
+//		return _instance;
+//	}
+//protected:
+//	Singleton()
+//	{
+//		cout << "Create SIngleton Obj." <<endl;
+//	}
+//private:
+//	static Singleton* _instance;
+//};
+//Singleton* Singleton::_instance = nullptr;
+//void thread_fun(int n)
+//{
+//	for (int i = 0; i < n; ++i)
+//	{
+//		Singleton *ps = Singleton::Instance();
+//		cout << "ps = " << ps << endl;
+//	}
+//}
+//void main()
+//{
+//	thread t1(thread_fun, 10);
+//	thread t2(thread_fun, 10);
+//	t1.join();
+//	t2.join();
+//}
+
+//void main()
+//{
+//	system("pause");
+//	Singleton *pst = Singleton::Instance();
+//	Singleton *pst1 = Singleton::Instance();
+//	Singleton *pst2 = Singleton::Instance();
+//	Singleton *pst3 = Singleton::Instance();
+//}
+
+//class Singleton
+//{
+//public:
+//	static Singleton* GetInstance()
+//	{
+//		return &m_instance;
+//	}
+//private:
+//	Singleton()
+//	{
+//		cout << "Create Singleton Obj." << endl;
+//	}
+//
+//	Singleton(Singleton const&);
+//	Singleton& operator=(Singleton const&);
+//	static Singleton m_instance;
+//};
+//Singleton Singleton::m_instance;
+//void main()
+//{
+//	Singleton *ps = Singleton::GetInstance();
+//	Singleton *ps1 = Singleton::GetInstance();
+//	Singleton *ps2 = Singleton::GetInstance();
+//	system("pause");
+//}
+//class noninherit final
+//{
+//public:
+//};
+//class Test : noninherit	//不能继承
+//{};
+//void main()
+//{
+//	Test t;
+//}
+//class noninherit
+//{
+//public:
+//	static noninherit GetInstace()
+//	{
+//		return noninherit();
+//	}
+//private:
+//	noninherit()
+//	{}
+//};
+//class Test : public noninherit
+//{};
+//void main()
+//{
+//	Test t;		//不允许
+//}
+
+//class noncopyable
+//{
+//protected:
+//	noncopyable(){}
+//	~noncopyable(){}
+//private:
+//	noncopyable(const noncopyable&);
+//	const noncopyable& operator=(const noncopyable&);
+//};
+//class Test : public noncopyable
+//{
+//public:
+//	Test(int data = 0) :m_data(data)
+//	{}
+//	Test(const Test& t)
+//	{
+//		m_data = t.m_data;
+//	}
+//private:
+//	int m_data;
+//};
+//
+//void main()
+//{
+//	Test t1;
+//	Test t2 = t1;
+//	Test t3;
+//	t3 = t1;
+//}
+
+//class StackOnly
+//{
+//public:
+//	static StackOnly CreateObject()
+//	{
+//		return StackOnly();
+//	}
+//private:
+//	StackOnly() {};
+//};
+//void main()
+//{
+//	//StackOnly *psk = new StackOnly;		//不允许new对象
+//	StackOnly sk = StackOnly::CreateObject();
+//}
+
+//class Test
+//{
+//public:
+//	static Test* GetInstance()
+//	{
+//		return new Test;
+//	}
+//public:
+//	void fun()
+//	{
+//		cout << "Test::fun()" << endl;
+//	}
+//private:
+//	Test()
+//	{}
+//private:
+//	int m_data = 0;
+//};
+//void main()
+//{
+//	Test *pt = Test::GetInstance();
+//	pt->fun();
+//}
 
 //FILE* OpenFile(const char *path)
 //{
