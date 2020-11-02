f8fa42578dd4ada3d9e41b975176803e1422303b
Missingrakan <3047451493@qq.com>
2020-11-02 16:29:32 +0800

    更新代码

--- review/test_11_1/test_11_1/Vector.h
+++ review/test_11_1/test_11_1/Vector.h
@@ -30,22 +30,23 @@ namespace bit
 		template<class Iterator>
 		vector(Iterator first, Iterator last)
 		{
+			
 			//确定该区间有多少个元素
 			Iterator it = first;
 			size_t count = 0;
 			while (it != last)
 			{
-				++count;
+				count++;
 				++it;
 			}
 
 			//给当前对象开辟空间，并初始化成员变量
-			start = new[count];
+			start = new T[count];
 			finish = start;
 			endofstorage = start + count;
 
 			//赋值
-			while (first != last);
+			while (first != last)
 			{
 				*finish++ = *first++;
 			}
@@ -60,7 +61,7 @@ namespace bit
 				start[i] = v[i];
 		}
 
-		vector<T>& vector(vector<T> v)
+		vector<T>& operator=(vector<T> v)
 		{
 			swap(v);
 			return *this;
@@ -101,12 +102,42 @@ namespace bit
 
 		void resize(size_t newsize, const T& data = T())
 		{
+			size_t oldsize = size();
+			if (newsize <= oldsize)
+				finish = start + newsize;
+			else
+			{
+				if (newsize > capacity())
+					reserve(newsize);
 
+				for (size_t i = oldsize; i < newsize; ++i)
+					start[i] = data;
+
+				finish = start + newsize;
+			}
 		}
 
 		void reserve(size_t newcapacity)
 		{
+			size_t oldsize = size();
+			size_t oldcapacity = capacity();
 
+			if (newcapacity > oldcapacity)
+			{
+				//开辟空间
+				T* temp = new T[newcapacity];
+
+				//拷贝元素
+				for (size_t i = 0; i < oldsize; ++i)
+					temp[i] = start[i];
+
+				//释放旧空间
+				delete[] start;
+
+				start = temp;
+				finish = start + oldsize;
+				endofstorage = start + newcapacity;
+			}
 		}
 	public:
 		T& operator[](size_t index)
@@ -148,15 +179,189 @@ namespace bit
 
 			*finish++ = data;
 		}
+
 		void pop_back()
 		{
 			if (empty())
 				return;
-			finish--;
+			--finish;
+		}
+
+		iterator insert(iterator pos, const T& data)
+		{
+			assert(pos <= end());
+
+			if (size() == capacity())
+				reserve(capacity() * 2);
+
+			//移动元素
+			auto it = end();
+			while (it > pos)
+			{
+				*it = *(it - 1);
+				--it;
+			}
+
+			//放置元素
+			*pos = data;
+			++finish;
+			return pos;
+		}
+
+		iterator erase(iterator pos)
+		{
+			assert(pos < end());
+
+			if (size() == 1)
+			{
+				pop_back();
+				return end();
+			}
+
+			auto it = pos;
+			while (it < end())
+			{
+				*it = *(it + 1);
+				++it;
+			}
+
+			--finish;
+			return pos;
+		}
+
+		void clear()
+		{
+			finish = start;
+		}
+
+		void swap(const vector<T>& v)
+		{
+			std::swap(start, v.start);
+			std::swap(finish, v.finish);
+			std::swap(endofstorage, v.endofstorage);
 		}
 	private:
 		iterator start;
 		iterator finish;
 		iterator endofstorage;
 	};
+}
+
+#include <iostream>
+
+using namespace std;
+
+#include <string>
+
+void TestMyVector1()
+{
+	bit::vector<int> v1;
+
+	bit::vector<int> v2(10, 5);
+
+	for (size_t i = 0; i < v2.size(); ++i)
+		cout << v2[i] << " ";
+	cout << endl;
+
+	int array[] = { 1, 2, 3, 4, 5 };
+	bit::vector<int> v3(array, array + 5);
+
+	auto it = v3.begin();
+	while (it != v3.end())
+	{
+		cout << *it << " ";
+		++it;
+	}
+	cout << endl;
+
+	std::string s("hello");
+	bit::vector<int> vc(s.begin(), s.end());
+	bit::vector<int> v4(v3);
+
+	for (auto e : v4)
+		cout << e << " ";
+	cout << endl;
+}
+
+void TestMyVector2()
+{
+	bit::vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+	v.push_back(4);
+
+	for (auto e : v)
+		cout << e << " ";
+	cout << endl;
+
+	v.pop_back();
+	v.pop_back();
+
+	for (auto e : v)
+		cout << e << " ";
+	cout << endl;
+
+	v.insert(v.begin(), 0);
+	for (auto e : v)
+		cout << e << " ";
+	cout << endl;
+
+	v.erase(v.begin());
+	for (auto e : v)
+		cout << e << " ";
+	cout << endl;
+
+	v.clear();
+	cout << v.size() << endl;
+	cout << v.capacity() << endl;
+}
+
+void TestMyVector3()
+{
+	bit::vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+	v.push_back(4);
+
+	cout << v[0] << endl;
+	cout << v.front() << endl;
+	cout << v.back() << endl;
+
+	// capacity:9  size: 4
+	v.resize(8, 5);
+	cout << v.size() << endl;
+	cout << v.capacity() << endl;
+	for (auto e : v)
+		cout << e << " ";
+	cout << endl;
+
+	v.resize(15, 6);
+	cout << v.size() << endl;
+	cout << v.capacity() << endl;
+	for (auto e : v)
+		cout << e << " ";
+	cout << endl;
+
+	v.resize(20, 6);
+	cout << v.size() << endl;
+	cout << v.capacity() << endl;
+	for (auto e : v)
+		cout << e << " ";
+	cout << endl;
+
+	v.resize(12);
+	cout << v.size() << endl;
+	cout << v.capacity() << endl;
+	for (auto e : v)
+		cout << e << " ";
+	cout << endl;
+
+	v.resize(5);
+	cout << v.size() << endl;
+	cout << v.capacity() << endl;
+	for (auto e : v)
+		cout << e << " ";
+	cout << endl;
 }
--- review/test_11_1/test_11_1/test.cpp
+++ review/test_11_1/test_11_1/test.cpp
@@ -1,2 +1,67 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void TestVector(size_t n)
+{
+	vector<vector<int>> vv;
+
+	//开辟每行保存vector元素的空间
+	vv.resize(n);
+
+	//开辟每行的空间
+	for (size_t i = 0; i < n; ++i)
+	{
+		vv[i].resize(i + 1, 1);
+	}
+
+	//计算杨辉三角除第0列和对角线之外的元素
+	for (size_t i = 2; i < n; ++i)
+	{
+		for (size_t j = 1; j < i; ++j)
+			vv[i][j] = vv[i - 1][j] + vv[i - 1][j - 1];
+	}
+
+	for (size_t i = 0; i < n; ++i)
+	{
+		for (size_t j = 0; j < vv[i].size(); ++j)
+		{
+			printf("%3d",vv[i][j]);
+		}
+
+		printf("\n");
+	}
+}
+
+int main()
+{
+	TestVector(5);
+	return 0;
+}
+
+#if 0
+int main()
+{
+	vector<int> v;
+	v.push_back(1);
+	auto it = v.erase(v.begin());
+	if (it == v.end())
+		cout << "it == end" << endl;
+	else
+		cout << "it != end" << endl;
+	return 0;
+	return 0;
+}
+
+#include "Vector.h"
+int main()
+{
+	//TestMyVector1();
+	//TestMyVector2();
+	TestMyVector3();
+	return 0;
+}
+
+#endif
